0c104b1298906da1095c774980a2c1fb0efe2431
artumarinn <artumarin.09@gmail.com>
2025-03-24 20:57:48 -0500

    Path Parameters with types

--- main.py
+++ main.py
@@ -8,6 +8,26 @@ async def root():
     return {"message": "Hello World"}"
 """
 
+# Parametros de ruta - Path Parameters
+"""
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+"""
+# Parametros de ruta con tipos - Path Parameters with types
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id} 
+
+# Orden de las rutas
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
